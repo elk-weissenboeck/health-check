68fd39eb7562056a45dc9e68e0e80a39fac28ff8
georgw <georg.weissenboeck@elk.at>
2025-11-12 15:12:34 +0100

    add data

--- public/entra/serviceOwners.php
+++ public/entra/serviceOwners.php
@@ -4,6 +4,16 @@
 // 'durchwahl' always taken from here
 //
 return [
+    'info@icomedias.com' => [
+        'name' => 'Icomedias GmbH', 
+        'email' => 'info@icomedias.com', 
+        'mobileExt' => '+43 (316) 721 671-0'
+    ],
+    'info@levatis.com' => [
+        'name' => 'levatis Software Service GmbH', 
+        'email' => 'info@icomedias.com', 
+        'mobileExt' => '+43 (0) 5 700 900'
+    ],
     'support@levatis.com' => [
         'name' => 'Hr. Weiss', 
         'email' => 'support@levatis.com', 
@@ -23,5 +33,10 @@ return [
         'name' => 'Simon Fach', 
         'email' => 's.fach@q23.de', 
         'mobileExt' => '+49 68 23 16 33'
+    ],
+    'info@visionme.de' => [
+        'name' => 'VISIONME GmbH', 
+        'email' => 'info@visionme.de', 
+        'mobileExt' => '+49 (0)511 165 889 10'
     ]
 ];
